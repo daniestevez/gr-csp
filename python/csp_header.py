a3e886c50f3972ae1f64ac402e0a45aff6d45c41
Daniel Estévez <daniel@destevez.net>
2016-05-09 11:23:48 +0200

    Added python class for CSP header

---
+++ python/csp_header.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2016 Daniel Estevez <daniel@destevez.net>.
+
+# This is free and unencumbered software released into the public domain.
+
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+# For more information, please refer to <http://unlicense.org>
+
+import struct
+
+class CSP(object):
+    def __init__(self, csp_packet):
+        csp = struct.unpack("<I", csp_packet[0:4])[0]
+        self.priority = (csp >> 30) & 0x3
+        self.source = (csp >> 25) & 0x1f
+        self.destination = (csp >> 20) & 0x1f
+        self.dest_port = (csp >> 14) & 0x3f
+        self.source_port = (csp >> 8) & 0x3f
+        self.reserved = (csp >> 4) & 0xf
+        self.hmac = (csp >> 3) & 1
+        self.xtea = (csp >> 2) & 1
+        self.rdp = (csp >> 1) & 1
+        self.crc = csp & 1
+
+    def __str__(self):
+        return ("""CSP header:
+        Priority:\t\t\t{}
+        Source:\t\t\t{}
+        Destination:\t\t{}
+        Destination port:\t{}
+        Source port:\t\t{}
+        Reserved field:\t{}
+        HMAC:\t\t\t{}
+        XTEA:\t\t\t{}
+        RDP:\t\t\t{}
+        CRC:\t\t\t{}""".format(
+            self.priority, self.source, self.destination, self.dest_port,
+            self.source_port, self.reserved, self.hmac, self.xtea, self.rdp,
+            self.crc))
