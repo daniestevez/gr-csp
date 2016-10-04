de19c0bda416461e0e2c8e52c99cd8d442930e4e
Daniel Estévez <daniel@destevez.net>
2016-10-04 16:16:55 +0200

    Added some missing copyright statements

--- python/swap_crc.py
+++ python/swap_crc.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright 2016 Daniel Estevez <daniel@destevez.net>.
+#
 # This is free and unencumbered software released into the public domain.
 # 
 # Anyone is free to copy, modify, publish, use, compile, sell, or
